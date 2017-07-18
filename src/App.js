0ba6b164df13c7e38acb06ebec983393f265ce91
Chase Sang <chase.sang@gmail.com>
2017-07-17 22:09:46 -0700

    CSS change

--- src/App.js
+++ src/App.js
@@ -18,19 +18,19 @@ const educationData = props.jsonObj.education;
     return (
       <div className="container">
              <div className="row">
-               <aside className="col-md-4">
+
                  <div className="inner">
                    <Profile profileData={profileData} />
                  </div>
-               </aside>
-               <main className="col-md-8">
+
+
                 <div className="inner">
                 <Summary summaryData={summaryData} />
                 <Work workData={workData} />
                 <TechnicalSkills skillsData={skillsData} />
                 <Education educationData={educationData} />
                  </div>
-               </main>
+
              </div>
            </div>
     );
