55658b9ae31d2df893af600b1567da7900b36944
Chase Sang <chase.sang@gmail.com>
2017-07-19 23:13:33 -0700

    updates-unknown

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Summary from './components/Summary';
 import Work from './components/Work';
 import TechnicalSkills from './components/TechnicalSkills';
 import Education from './components/Education';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const App = props => {
 const profileData = props.jsonObj.basics;
@@ -17,7 +18,7 @@ const educationData = props.jsonObj.education;
     return (
       <div className="container">
         <div className="row"> <h1>Chase Sang</h1> </div>
-        <h2> About |  Engineering  |  Web Development  |  Hangouts  |  Destinations  |  Fun  | Thought Sketches </h2>  
+        <h2> About |  Engineering  |  Web Development  |  Hangouts  |  Destinations  |  Fun  | Thought Sketches </h2>
              <div className="row">
                <aside className="col-md-4">
                  <div className="inner">
@@ -39,5 +40,67 @@ const educationData = props.jsonObj.education;
 App.propTypes = {
     jsonObj: PropTypes.object.isRequired
 }
+const Appp = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><Link to="/topics">Topics</Link></li>
+      </ul>
 
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+)
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
 export default App;
